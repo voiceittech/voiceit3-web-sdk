e6407d09d8d56d3df8e40db391ac2c84d480445d
Noel Grover <noel.grover@gmail.com>
2018-07-13 16:49:53 -0500

    Removed Files

--- credentials.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	API_TOKEN: 'API_KEY_HERE',
-	API_KEY: 'API_TOKEN_HERE'
-}
--- public/node_modules/videojs-record/dist/lang/en.js
+++
@@ -1,7 +0,0 @@
-videojs.addLanguage('en', {
-  "Device": "Device",
-  "Record": "Record",
-  "Stop": "Stop",
-  "Image": "Image",
-  "Retry": "Retry"
-});
