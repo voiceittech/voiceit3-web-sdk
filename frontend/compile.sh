33f22f6f829e6413b9534676116fa18e9789d0a0
Noel Grover <noel.grover@gmail.com>
2021-07-23 18:48:53 -0700

    vulnerability fixes and webpack updates

--- frontend/compile.sh
+++ frontend/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yarn build
+npm run build
 cp dist/voiceit2.min.js ../dist/voiceit2.min.js
 cp dist/voiceit2.min.js ../node-server-example/public/js/voiceit2.min.js
 cp dist/voiceit2.min.js ../php-server-example/js/voiceit2.min.js
